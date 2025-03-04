f681a5e21cc023847b8b580fe09fe9542b3a7100
Sebastian11082003 <juanchosorio845@gmail.com>
2025-03-04 03:55:58 -0500

    docs(readme): Update

--- src/products-module/products-module.service.ts
+++ src/products-module/products-module.service.ts
@@ -1,5 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import * as fs from 'fs';
+import * as path from 'path';
 
 
 @Injectable()
-export class ProductsModuleService {}
+export class ProductsModuleService {
+    private products = []
+
+    constructor(){
+        // cargar los datos desde el archibo JSON
+        try {
+            const filePath = path.join(process.cwd(), 'src/Apis/products.json');
+            console.log('Loading products data from:', filePath);
+
+            const jsonData = fs.readFileSync(filePath, 'utf-8');
+
+                if (!jsonData) {
+                    throw new Error ('No data found in the file');
+                }
+               
+            this.products = JSON.parse(jsonData);
+        }catch(error){
+            console.error ('Error loading products data:', error);
+            this.products = [];
+        }
+    }
+
+    findAll(){
+        return this.products;
+    }
+
+    findOneById(id: number){
+        return this.products.find(product => Number (product) === Number (id));
+    }
+}
